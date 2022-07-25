58aed2c3d6a8dd30f89b8b60daf4d6fb6bc126c1
fadhilahm <fadhilahmetra@gmail.com>
2022-07-25 16:54:24 +0900

    Added test for saving deck from and into file.

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
@@ -15,4 +18,20 @@ func TestNewDeck(t *testing.T) {
 	if d[len(d)-1] != "King of Spades" {
 		t.Errorf("Expected last card to be 'King of Spades', but got %v.", d[len(d)-1])
 	}
+}
+
+func TestSaveToFileAndReadDeckFromFile(t *testing.T) {
+	testFileName := "_decktesting"
+
+	os.Remove(testFileName)
+
+	d := newDeck()
+	d.saveToFile(testFileName)
+
+	loadedDeck := readDeckFromFile(testFileName)
+	if len(loadedDeck) != 52 {
+		t.Errorf("Expected deck length of 52, but got %v.", len(loadedDeck))
+	}
+
+	os.Remove(testFileName)
 }
